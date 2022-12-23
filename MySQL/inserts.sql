b629ecd3c83dc34f424bc112fd00a03db161e111
PauR03 <priusnori03@gmail.com>
2022-12-23 09:52:15 +0100

    mysql

---
+++ MySQL/inserts.sql
@@ -0,0 +1,194 @@
+use proyecto;
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',1,1,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',2,2,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',3,3,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',4,4,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',5,5,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',6,6,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',7,7,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,8,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,9,'oros');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,10,'oros');
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',1,1,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',2,2,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',3,3,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',4,4,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',5,5,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',6,6,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',7,7,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,8,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,9,'bastos');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,10,'bastos');
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',1,1,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',2,2,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',3,3,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',4,4,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',5,5,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',6,6,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',7,7,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,8,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,9,'espadas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,10,'espadas');
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',1,1,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',2,2,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',3,3,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',4,4,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',5,5,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',6,6,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',7,7,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,8,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,9,'copas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('española',0.5,10,'copas');
+
+
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',1,1,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',2,2,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',3,3,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',4,4,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',5,5,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',6,6,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',7,7,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,8,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,9,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,10,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,11,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,12,'picas');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,13,'picas');
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',1,1,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',2,2,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',3,3,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',4,4,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',5,5,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',6,6,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',7,7,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,8,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,9,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,10,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,11,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,12,'treboles');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,13,'treboles');
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',1,1,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',2,2,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',3,3,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',4,4,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',5,5,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',6,6,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',7,7,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,8,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,9,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,10,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,11,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,12,'diamantes');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,13,'diamantes');
+
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',1,1,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',2,2,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',3,3,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',4,4,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',5,5,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',6,6,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',7,7,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,8,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,9,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,10,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,11,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,12,'corazones');
+insert into cards (type_deck,value_in_game,cards_true_value,deck_suits)
+values('poker',0.5,13,'corazones');
